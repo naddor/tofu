4772b553009cd818e2c62f99281cbd4bb20436dd
Nans Addor <n.addor@uea.ac.uk>
2018-03-15 12:00:53 +0000

    moved dates defining the simulation periods from input_info to filemanager

--- create_forcing_nc.R
+++ create_forcing_nc.R
@@ -36,3 +36,43 @@ write_input_file_nc<-function(temp,prec,pet,q_obs,t_input,
   nc_close(nc_conn)
 
 }
+
+write_input_info<-function(file_name,nc_input_file){
+
+  content<-paste('! used to define information for the forcing file
+! -----------------------------------------------------------------------------------------------------------
+! lines starting with exclamation marks are not read
+! (variables can be in any order)
+! -----------------------------------------------------------------------------------------------------------
+<version>         FORCINGINFO.VERSION.2.1              ! string to ensure version of file matches the code
+<forcefile>       ',nc_input_file,'! name of data file
+<vname_iy>        undefined                            ! name of variable for year
+<vname_im>        undefined                            ! name of variable for month
+<vname_id>        undefined                            ! name of variable for day
+<vname_ih>        undefined                            ! name of variable for hour
+<vname_imin>      undefined                            ! name of variable for minute
+<vname_dsec>      undefined                            ! name of variable for second
+<vname_dtime>     time                                 ! time since reference time
+<vname_aprecip>   pr                                   ! variable name: precipitation
+<vname_airtemp>   temp                                 ! variable name: temperature
+<vname_spechum>   undefined                            ! variable name: specific humidity
+<vname_airpres>   undefined                            ! variable name: surface pressure
+<vname_swdown>    undefined                            ! variable name: downward shortwave radiation
+<vname_potevap>   pet                                  ! variable name: potential ET
+<vname_q>         q_obs                                ! variable name: runoff
+<units_aprecip>   mm/d                                 ! units: precipitation
+<units_airtemp>   degC                                 ! units: temperature
+<units_spechum>   undefined                            ! units: specific humidity
+<units_airpres>   undefined                            ! units: surface pressure
+<units_swdown>    undefined                            ! units: downward shortwave radiation
+<units_potevap>   mm/d                                 ! units: potential ET
+<units_q>         mm/d                                 ! units: runoff
+<deltim>          1.0                                  ! time step (days)
+<xlon>            -75.00                               ! longitude
+<ylat>              4.00                               ! latitude',sep='')
+
+  fileConn<-file(file_name)
+  writeLines(content, fileConn)
+  close(fileConn)
+
+}
--- setup_fuse_camels.R
+++ setup_fuse_camels.R
@@ -1,8 +1,6 @@
 rm(list=ls())
 
-require(RColorBrewer)
-
-### IDENTIFY COMPUTER AND SET WORKING DIRECTORIES
+# identify computer and set directories
 hostname<-system('hostname',intern=TRUE)
 
 if(hostname=='hydro-c1'){
@@ -13,60 +11,104 @@ if(hostname=='hydro-c1'){
 
 } else {
 
-  stop('You are not running this script from your usual computer')
+  stop('You are not running this script from hydro!')
 
 }
 
 # set directories for FUSE files
-dir_fuse_input_ch<-paste0('/glade/scratch/naddor/fusex/',country,'/input/')
-dir_fuse_output_ch<-paste0('/glade/scratch/naddor/fusex/',country,'/output/')
-dir_fuse_settings_ch<-paste0('/glade/scratch/naddor/fusex/',country,'/settings/')
+dir_fuse_bin<-'/home/naddor/fuse/bin/'
+dir_fuse_files<-'/d7/naddor/fuse/param_transfer_maurer/'
+dir_input<-paste0(dir_fuse_files,'input/'); if(!dir.exists(dir_input)){dir.create(dir_input)}
+dir_output<-paste0(dir_fuse_files,'output/'); if(!dir.exists(dir_output)){dir.create(dir_output)}
+dir_settings<-paste0(dir_fuse_files,'settings/'); if(!dir.exists(dir_settings)){dir.create(dir_settings)}
 
 # load functions
 source(paste(dir_r_scripts,'tofu/create_forcing_nc.R',sep=''))
 source(paste(dir_r_scripts,'tofu/create_elev_bands_nc.R',sep=''))
 source(paste(dir_r_scripts,'tofu/create_settings.R',sep=''))
-source(paste(dir_r_scripts,'read_catchment_data.R',sep='')) # move this file to CAMELS repo?
+source(paste(dir_r_scripts,'camels/read_camels_hydromet.R',sep=''))
 
-### FORCING PERIOD TO EXTRACT
-date_start_forcing='19801001'
-date_end_forcing='20140930'
+# load ET functions
+dir_my_functions<-paste0(dir_r_scripts,'my_functions/') # needed in my_functions_pot_evap
+source(paste(dir_r_scripts,'my_functions/my_functions_pot_evap.R',sep=''))
 
-### WARM-UP AND CALIBRATION PERIOD - BENCHMARK STUDY
-date_start_sim='19900101'
-date_end_spinup='19990930'
-date_end_sim='20080930'
+# FUSE setup
+country<-'us'
+fuse_id<-902 # VIC WITH SNOW
 
-### CREATE INPUT AND SETTING FILES FOR ALL CATCHMENTS
+# define period for which to extract forcing
+date_start_forcing<-'19801001' # start of Maurer time series for CAMELS basins
+date_end_forcing<-'20081231'   # end of Maurer time series for CAMELS basins
+forcing_dataset<-'maurer'
+
+# define period for the hydrological simulations
+# Andy: "The calibration period was WY2000-2008 and validation was WY1990-1999.
+# For calibration,we started the model 1 January 1990 and let it spin up for 10 years.
+# For validation, we started the model at 1 January 1980 and let it spin up for 10 years."
+
+for (exp_name in c('cal','val')){
+
+  if(exp_name=='cal'){
+
+    # Calibration benchmark study
+    date_start_sim<-'1990-01-01'
+    date_end_sim<-'2008-09-30'
+    date_start_eval<-'1999-10-01'
+    date_end_eval<-'2008-09-30'   # same as date_end_sim
+
+  }else{
+
+    # Evaluation benchmark study
+    date_start_sim<-'1980-01-01'
+    date_end_sim<-'1999-09-30'
+    date_start_eval<-'1989-10-01'
+    date_end_eval<-'1999-09-30'   # same as date_end_sim
+
+  }
+
+  # create file manager files
+  file_name<-paste0(dir_fuse_bin,'fm_',fuse_id,'_benchmark_',exp_name,'.txt')
+
+  write_file_manager(file_name,dir_input,dir_output,dir_settings,fuse_id,
+                     date_start_sim,date_end_sim,date_start_eval,date_end_eval)
+
+}
+
+# loop through CAMELS catchments
 for(i in 1:dim(camels_name)[1]){
 
-  id=camels_name$gauge_id[i]
-  huc=camels_name$huc_02[i]
+  id<-camels_name$gauge_id[i]
+  huc<-camels_name$huc_02[i]
 
   print(paste(i,'-',camels_name$gauge_name[i]))
 
-  ### CREATE INPUT
-  dat<-create_fuse_input_files(id,huc,country='us',
-                              gauge_lat=camels_topo$gauge_lat[i],gauge_lon=camels_topo$gauge_lat[i],
-                              date_start_forcing,date_end_forcing,
-                              date_start_sim,date_end_spinup,date_end_sim,
-                              return_hs_ci=FALSE,
-                              return_frac_avail_qobs=TRUE,breaks_hydro_years=breaks_hydro_years,
-                              list_fuse_id=list_fuse_id)
+  # retrieve hydrometeorological time series
+  input_table<-get_catchment_data_dataframe(huc,id,date_start_forcing,date_end_forcing,forcing_dataset)
+  t_input<-as.Date(input_table$date,format='%Y%m%d')
+  prec<-input_table$prec
+  temp<-(input_table$temp_min+input_table$temp_max)/2
+  q_obs<-input_table$q_obs
+  pet<-input_table$pet
+
+  # compute PET using Oudin
+  # pet_o_harm<-pet_oudin(temp=temp,d=t_input,lat=gauge_lat,'harm')
+  # pet_o_mov_window<-pet_oudin(temp=temp,d=t_input,lat=gauge_lat,'mov_window')
+
+  # save forcing to NetCDF file
+  name_input_file<-paste(country,'_',id,'_',forcing_dataset,'_input.nc',sep='')
+
+  write_input_file_nc(temp,prec,pet,q_obs,t_input,
+                      gauge_lat=-camels_topo$gauge_lat[i],gauge_lon=camels_topo$gauge_lon[i],
+                      na_value=-9999,
+                      dir_input,name_input_file)
+
+  name_input_info_file<-paste(country,'_',id,'_',forcing_dataset,'_input_info.txt',sep='')
+
+  write_input_info(paste0(dir_settings,name_input_info_file),nc_input_file=name_input_file)
+
   # ELEV BANDS
   create_elevation_bands()
 
-  # SETTINGS
-  file_input_info<-paste(dir_settings,country,'_',id,'_input_info.txt',sep='')
-  write_fuse_input_info(file_input_info,nc_input_file=name_forcing_file,i_start_sim,i_end_spinup,i_end_sim,
-                        longrun_beg=i_start_longrun,longrun_end=i_end_longrun,numtim_sub)
-
-  for(fuse_id in list_fuse_id){
-
-    file_file_manager<-paste(dir_settings,country,'_',id,'_',fuse_id,'_fuse_file_manager.txt',sep='')
-    write_fuse_file_manager(file_file_manager,dir_fuse_input_ch,dir_fuse_output_ch,dir_fuse_settings_ch,fuse_id=fuse_id)
-
-  }
 
 }
 
@@ -98,7 +140,6 @@ write('#!\\bin\\sh', name_batch_file_def, append=TRUE)
 write('#!\\bin\\sh', name_batch_file_sce, append=TRUE)
 write('#!\\bin\\sh', name_batch_file_best, append=TRUE)
 
-fuse_id=902 # VIC WITH SNOW
 
 for(id in id_us){
 
