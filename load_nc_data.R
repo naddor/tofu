ec95085d60f05bdfadf49a14a85c628c388efcea
Nans Addor <n.addor@uea.ac.uk>
2019-05-30 10:02:12 +0100

    now takes names of variables to extract as arugments

--- load_nc_data.R
+++ load_nc_data.R
@@ -29,7 +29,16 @@ read_nc_input<-function(path_to_nc_file){
 
 }
 
-read_nc_output<-function(path_to_nc_file){
+read_nc_output<-function(path_to_nc_file,var_to_extract=c('q_obs','q_routed')){
+
+  # options for var_to_extract: 'qobs','qsim','q_instant','swe'
+  # date extracted and saved as global variable regardless
+
+  if(any(!var_to_extract%in%c('q_obs','q_routed','q_instant','swe'))){
+
+    stop('Variables to extract can only be: q_obs, q_routed, q_instant, swe')
+
+  }
 
   # open file
   nc_id<-nc_open(path_to_nc_file)
@@ -48,10 +57,12 @@ read_nc_output<-function(path_to_nc_file){
   d_origin<-as.Date(d_unit_split[3],'%Y-%m-%d')
   d_output<<-d_origin+d_raw
 
-  # extract variables and store them in global environment
-  qobs<<-ncvar_get(nc_id,'obsq')
-  qsim<<-ncvar_get(nc_id,'q_routed')
-  swe<<-ncvar_get(nc_id,'swe_tot')
+  # extract required variables and store them in global environment
+  # variables not required are overwritten and set to NA
+  if('q_obs'%in%var_to_extract){q_obs<<-ncvar_get(nc_id,'obsq')}else{qobs<<-NA}
+  if('q_routed'%in%var_to_extract){q_routed<<-ncvar_get(nc_id,'q_routed')}else{qsim<<-NA}
+  if('q_instant'%in%var_to_extract){q_instant<<-ncvar_get(nc_id,'q_instnt')}else{q_instant<<-NA}
+  if('swe'%in%var_to_extract){swe<<-ncvar_get(nc_id,'swe_tot')}else{swe<<-NA}
 
   nc_close(nc_id)
 
